538756c60c7c962b7d90a93d9da3da450b7c0123
renatakhabirva <renatahabirovka49@gmail.com>
2024-12-09 17:41:17 +0500

    Добавьте файлы проекта.

---
+++ ServicePage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharafutdinovGlazkiSave2
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public ServicePage()
+        {
+            InitializeComponent();
+            var currentServices = ШарафутдиновГлазкиSaveEntities.GetContext().Agent.ToList();
+            ServiceListView.ItemsSource = currentServices;
+            ComboType.SelectedIndex = 0;
+            UpdateServices();
+        }
+
+        private void UpdateServices()
+        {
+            var currentServices = ШарафутдиновГлазкиSaveEntities.GetContext().Agent.ToList();
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Phone.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "").Contains(TBoxSearch.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", ""))).ToList();
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "МФО")).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "ООО")).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "ЗАО")).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "МКК")).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "ОАО")).ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeText.ToString() == "ПАО")).ToList();
+            }
+
+            if (SortType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.OrderBy(p => p.Title).ToList();
+            }
+            if (SortType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+            }
+            if (SortType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.OrderBy(p => p.Priority).ToList();
+            }
+            if (SortType.SelectedIndex == 6)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Priority).ToList();
+            }
+
+            ServiceListView.ItemsSource = currentServices.ToList();
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void DiscountButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void SortType_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+    }
+}
