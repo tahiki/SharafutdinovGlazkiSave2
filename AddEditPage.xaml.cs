31a6804000959af131c1b8b0b5ac2427095b170f
tahiki <98966640+tahiki@users.noreply.github.com>
2024-12-12 15:07:40 +0500

    Add files via upload

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,105 @@ namespace SharafutdinovGlazkiSave2
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Agent _currentAgent = new Agent();
+        public AddEditPage(Agent selectedAgent)
         {
             InitializeComponent();
+            if (selectedAgent != null)
+            {
+                this._currentAgent = selectedAgent;
+                ComboType.SelectedIndex = _currentAgent.AgentTypeID - 1;
+            }
+
+            DataContext = _currentAgent;
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                _currentAgent.Logo = myOpenFileDialog.FileName;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentAgent.Title))
+                errors.AppendLine("Укажите наименование агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Address))
+                errors.AppendLine("Укажите адрес агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.DirectorName))
+                errors.AppendLine("Укажите ФИО директора");
+            if (ComboType.SelectedItem == null)
+                errors.AppendLine("Укажите тип агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Priority.ToString()))
+                errors.AppendLine("Укажите приоритет агента");
+            if (_currentAgent.Priority <= 0)
+                errors.AppendLine("Укажите положительный приоритет агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.INN))
+                errors.AppendLine("Укажите ИНН агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.KPP))
+                errors.AppendLine("Укажите КПП агента");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Phone))
+                errors.AppendLine("Укажите телефон агента");
+            else
+            {
+                string ph = _currentAgent.Phone.Replace("(", "").Replace("-", "").Replace("+", "");
+                if (((ph[1] == '9' || ph[1] == '4' || ph[1] == '8') && ph.Length != 11)
+                    || (ph[1] == '3' && ph.Length != 12))
+                    errors.AppendLine("Укажите правильно телефон агента");
+            }
+
+            if (string.IsNullOrWhiteSpace(_currentAgent.Email))
+                errors.AppendLine("Укажите почту агента");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentAgent.ID == 0)
+                ШарафутдиновГлазкиSaveEntities.GetContext().Agent.Add(_currentAgent);
+            try
+            {
+                ШарафутдиновГлазкиSaveEntities.GetContext().Agent.Add(_currentAgent);
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var _currentAgent = (sender as Button).DataContext as Agent;
+
+            var currentProductSale = ШарафутдиновГлазкиSaveEntities.GetContext().ProductSale.ToList();
+            currentProductSale = currentProductSale.Where(p => p.AgentID == _currentAgent.ID).ToList();
+
+            var currentAgentPriorityHistory = ШарафутдиновГлазкиSaveEntities.GetContext().AgentPriorityHistory.ToList();
+            var currentShop = ШарафутдиновГлазкиSaveEntities.GetContext().Shop.ToList();
+            currentAgentPriorityHistory = currentAgentPriorityHistory.Where(p => p.AgentID == _currentAgent.ID).ToList();
+            currentShop = currentShop.Where(p => p.AgentID == _currentAgent.ID).ToList();
+
+            if (currentProductSale.Count != 0)
+                MessageBox.Show("Невозможно выпольнить удаление, т. к. существует история реализации продуктов");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+
+                    }
+                }
+            }
+
         }
     }
 }
